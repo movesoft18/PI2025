69dc2ad8c4877a67654421285cab561471490187
movesoft18 <movesoft@yandex.ru>
2025-12-25 16:49:09 +0400

    filesystem

---
+++ filesystem_example/FileSorter/FileSorter.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <filesystem>
+using namespace std::filesystem;
+using namespace std;
+
+void SortFiles(const path& folder)
+{
+    path images_dir = folder.parent_path();
+    path labels_dir = images_dir;
+    images_dir /= "images";
+    labels_dir /= "labels";
+    // создаем папки для изображений и аннотаций
+    create_directory(images_dir);
+    create_directory(labels_dir);
+
+    // сортировка
+    for (auto& file : directory_iterator(folder))
+    {
+        path curr_file = file.path(); // получаем тек. объект
+        if (exists(curr_file) &&
+            is_regular_file(curr_file)
+            ) // проверяем что он существует и это файл
+        {
+            bool res = false;
+            if (curr_file.extension() == ".jpg")
+            {
+                // копируем в папку images
+                path target_file = images_dir; // формируем имя
+                target_file /= curr_file.filename(); // результирующего файла
+                res = copy_file(curr_file, target_file,
+                    copy_options::overwrite_existing);
+
+            }
+            else if (curr_file.extension() == ".txt")
+            {
+                // копируем в папку labels
+                path target_file = labels_dir; // формируем имя
+                target_file /= curr_file.filename(); // результирующего файла
+                res = copy_file(curr_file, target_file,
+                    copy_options::overwrite_existing);
+            }
+            if (res)
+            {
+                cout << "Файл " << curr_file.filename() << " скопирован" << endl;
+                res = remove(curr_file);
+                if (res) cout << "Файл " << curr_file.filename() << " удален из исходной папки" << endl;
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    path curr_dir = current_path();
+    curr_dir /= "files";
+    SortFiles(curr_dir);
+}
---
+++ filesystem_example/filesystem_example/filesystem_example.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+bool isBMPFile(const string& filename)
+{
+    fs::path bmpFile(filename);
+    if (bmpFile.extension() == ".bmp")
+        return true;
+    return false;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    fs::path file_path("dir1/dir2/file.txt");
+    cout << file_path << endl; // Базовое имя файла
+    cout << file_path.parent_path() << endl; // Родительская папка
+    cout << file_path.filename() << endl; // Имя файла
+    cout << file_path.extension() << endl; // расширение
+
+    cout << isBMPFile("/dir1/dir2/file.b4mp") << endl;
+
+    file_path.replace_filename("file2"); // меняем имя файла
+    file_path.replace_extension("cpp");    // меняем расширение файла
+    cout << file_path << endl; // Новое имя
+
+    fs::path curr_path = fs::current_path(); // получаем имя текущей папки
+    cout << curr_path << endl; // текущая папка
+    //curr_path.append("filesystem_example.cpp"); // Добавляем новый фрагмент пути
+    curr_path /= "filesystem_example.cpp";
+    cout << curr_path << endl; // текущий файл кода
+
+    if (fs::exists(curr_path))
+    {
+        cout << "Файл " << curr_path.filename() << " существует\n";
+    }
+    auto stat = fs::status(curr_path);
+    if (fs::is_directory(curr_path.parent_path()))
+    {
+        cout << "Объект " << curr_path.parent_path() << " является папкой\n";
+
+    }
+    if (fs::is_regular_file(curr_path))
+    {
+        cout << "Объект " << curr_path.filename() << " является файлом\n";
+    }
+    fs::create_directory("dir");
+    fs::create_directories("dir/dir1/dir2/dir3");
+
+    //Нерекурсивный обход
+    cout << "\n\n\n";
+    for (auto& p : fs::directory_iterator(fs::current_path()))
+    {
+        cout << p.path() << endl;
+    }
+
+    //Pекурсивный обход
+    cout << "\n\n\n";
+    for (auto& p : fs::recursive_directory_iterator(fs::current_path()))
+    {
+        cout << p.path() << endl;
+    }
+    bool result = fs::copy_file(
+        curr_path,
+        "dir/dir1/dir2/dir3/filesystem.cpp",
+        fs::copy_options::overwrite_existing
+        );
+    cout << "Размер файла " << 
+        fs::file_size("dir/dir1/dir2/dir3/filesystem.cpp") << endl;
+    fs::rename("dir/dir1/dir2/dir3/filesystem.cpp", 
+        "dir/dir1/dir2/dir3/filesystem.bak");
+    fs::remove_all("dir");
+}
