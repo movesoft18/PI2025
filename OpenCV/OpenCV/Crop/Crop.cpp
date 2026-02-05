dc1fc87bd58ff0190468a6d61ba0a24bf56a4864
movesoft18 <movesoft@yandex.ru>
2026-02-05 08:46:09 +0400

    Crop

---
+++ OpenCV/OpenCV/Crop/Crop.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+int main()
+{
+    Mat image = imread("image.png");
+    imshow("Woman", image);
+    Mat crop = image(Rect(100, 100, 80, 150));
+    imwrite("cropped.png", crop);
+    imshow("Часть человека", crop);
+    waitKey();
+}
