6d3bde821deb36df682abfde3f4ecec51781633f
movesoft18 <movesoft@yandex.ru>
2026-02-10 15:19:45 +0400

    Filters

---
+++ OpenCV/OpenCV/ColorFinder/ColorFinder.cpp
@@ -0,0 +1,92 @@
+﻿/*Стандартный прием в OpenCV для решения поставленной задачи складывается из двух
+этапов. На первом этапе мы применяем цветовой фильтр и превращаем каждый кадр в
+черно - белую картинку.После наложения фильтра объект заданного цвета превращается
+в белое пятно, а всё остальное заливается черным цветом.
+На следующем этапе мы используем алгоритм вычисления моментов. Момент
+изображения — это суммарная характеристика пятна, представляющая собой сумму всех
+точек (пикселей) этого пятна. При этом, имеется множество подвидов моментов,
+характеризующие разные свойства изображения.
+
+Например, момент нулевого порядка m00 — это количество всех точек, составляющих
+пятно. Момент первого порядка m10 представляет собой сумму X координат точек,
+а m01 — сумму Y координат. Имеются также моменты m11, m20, m02, m22 и т.д.
+Формула для вычисления моментов очень проста, и мы можем посчитать пиксели вручную.
+Воспользуемся стандартной функцией для вычисления моментов кадра:
+
+moments( кадр, двоичный )
+
+Аргумент кадр представляет собой нашу предобработанную картинку.
+Аргумент двоичный определяет то, как алгоритм будет вычислять вес каждой точки.
+Напомню, что предыдущий метод inRange дал нам черно-белую картинку, в которой
+пиксели могут быть черными, белыми, а могут быть и серыми. Так вот, если аргумент
+двоичный равен 1, то вес всех точек с цветом, отличным от нуля будет равен единице.
+В противном случае, вес черной точки будет равен 0, а белой точки — 255.
+Функция moments вернет нам массив моментов вплоть до третьего порядка.
+Но для вычисления координат центра пятна нам потребуются только моменты первого
+порядка m01 и m10, а также момент m00.
+Чтобы получить координаты X и Y искомого пятна, нам следует поделить полученные
+моменты m10 и m01 на нулевой момент m00. Таким образом мы найдем средние координаты
+X и Y всех точек, а это и есть центр пятна.
+
+x = dM10 / dArea
+y = dM01 / dArea
+*/
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	char mainWindow[] = "Main";
+	char trackbarWindow[] = "Trackbar";
+	char sizeWindow[] = "Size";
+	char thresholdWindow[] = "Threshold";
+	int min = 0, max = 10000;
+	int hmin = 0, smin = 0, vmin = 0,
+		hmax = 180, smax = 255, vmax = 255,
+		x = 0, y = 0;
+	Mat frame, HSV, threshold;
+	namedWindow(trackbarWindow);
+	namedWindow(thresholdWindow);
+	namedWindow(mainWindow);
+	namedWindow(sizeWindow);
+	frame = imread("image.jpg");
+	createTrackbar("H min:", trackbarWindow, &hmin, hmax);
+	createTrackbar("H max:", trackbarWindow, &hmax, hmax);
+	createTrackbar("S min:", trackbarWindow, &smin, smax);
+	createTrackbar("S max:", trackbarWindow, &smax, smax);
+	createTrackbar("V min:", trackbarWindow, &vmin, vmax);
+	createTrackbar("V max:", trackbarWindow, &vmax, vmax);
+	createTrackbar("Size min:", sizeWindow, &min, max);
+	createTrackbar("Size max:", sizeWindow, &max, max);
+
+	for (;;) {
+
+		cvtColor(frame, HSV, COLOR_BGR2HSV);
+
+		inRange(HSV,
+			Scalar(hmin, smin, vmin),
+			Scalar(hmax, smax, vmax),
+			threshold);
+		Moments mom = moments(threshold, true);
+		double dM01 = mom.m01;
+		double dM10 = mom.m10;
+		double dArea = mom.m00;
+		//будем реагировать только на те моменты,
+		//которые содержать больше заданного кол-ва пикселей
+		if (dArea >= min && dArea < max)
+		{
+			x = dM10 / dArea;
+			y = dM01 / dArea;
+		}
+		imshow(mainWindow, frame);
+		imshow(thresholdWindow, threshold);
+		if (waitKey(1) == 27) break;
+	}
+	destroyAllWindows();
+}
+
---
+++ OpenCV/OpenCV/Filters/Filters.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+
+int main()
+{
+	Mat image = imread("eurobot.bmp");
+	imshow("Оригинал", image);
+	medianBlur(image, image, 3);
+	imshow("Отфильтровано", image);
+	Mat image1 = imread("image.jpg");
+	imshow("Оригинал2", image1);
+	medianBlur(image1, image1, 9);
+	imshow("Отфильтровано2", image1);
+	Mat image2 = imread("image2.jpg");
+	imshow("Оригинал3", image2);
+	GaussianBlur(image2, image2, Size(9,9), 0);
+	imshow("Отфильтровано3", image2);
+	Mat image3 = imread("eurobot.bmp");
+	imshow("Оригинал4", image3);
+	GaussianBlur(image3, image3, Size(3,3),0);
+	imshow("Отфильтровано4", image3);
+	waitKey();
+	destroyAllWindows();
+}
+
